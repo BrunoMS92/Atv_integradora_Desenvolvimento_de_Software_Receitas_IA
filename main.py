12a8393f4590ad1c472215a97757602f9a5699e7
BrunoMS92 <brunom_job@hotmail.com>
2026-06-04 19:07:48 -0300

    Implementando fluxo MVC e realizando um teste no terminal usando python main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from views.menu import pedir_receita
+from controllers.receita_controller import ReceitaController
+
+
+def main():
+
+    pedido = pedir_receita()
+
+    controller = ReceitaController()
+
+    receita = controller.criar_receita(pedido)
+
+    print("\nReceita criada:")
+    print(receita.conteudo)
+
+
+if __name__ == "__main__":
+    main()
