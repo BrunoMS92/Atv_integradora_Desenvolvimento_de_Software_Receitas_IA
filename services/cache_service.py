d7552bbae4b33fc3308b6a6592cda1ebdb934b0b
BrunoMS92 <brunom_job@hotmail.com>
2026-06-07 09:08:17 -0300

    Criei o método salvar cache para armazenar até 5 receitas no cache.json

--- services/cache_service.py
+++ services/cache_service.py
@@ -7,3 +7,18 @@ class CacheService:
 
         with open("cache.json", "r", encoding="utf-8") as arquivo:
             return json.load(arquivo)
+        
+    def salvar_cache(self, pedido):
+
+        receitas = self.ler_cache()
+
+        receitas.append(
+            {
+                "pedido": pedido
+            }
+        )
+
+        receitas = receitas[-5:]
+
+        with open("cache.json", "w", encoding="utf-8") as arquivo:
+            json.dump(receitas, arquivo, indent=4, ensure_ascii=False)
